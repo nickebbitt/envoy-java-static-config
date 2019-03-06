aab30012e43ade4a04d1a1d9c5fc37133614e88d
Nick Ebbitt <nickebbitt@gmail.com>
2019-03-06 16:59:57 +0000

    Add base apps

---
+++ message-generator/src/test/java/com/nickebbitt/messagegenerator/MessageGeneratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nickebbitt.messagegenerator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MessageGeneratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
